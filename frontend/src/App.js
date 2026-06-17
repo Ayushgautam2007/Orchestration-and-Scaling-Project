b2b8f14baa11cd772139c18c5294e360db279281
Ayush Gautam <gautamayush143@gmail.com>
2026-06-17 16:34:00 +0530

    Webhook test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,9 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
-     <Home></Home>
+      <h1>Welcome to CI/CD Pipeline 🚀</h1>
+      <h2>Jenkins + GitHub + ECR Test</h2>
+      <Home />
     </div>
   );
 }
